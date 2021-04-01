8ffeb8aa207613307736eddd05a9a84b0ac894b9
rpc08002 <rcurtin0491@gmail.com>
2021-04-01 14:33:17 -0400

    set up route paths

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from 'react'
 import Navbar from "./Navbar"
 import Home from "./Home"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Navbar />
-      <div className="content">
+      <div>
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/create">
-            <Create />
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
           </Route>
         </Switch>
       </div>
